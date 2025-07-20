b2df599cdfd7f445b12f0385175cd45c548a030f
setloger <setloger@gmail.com>
2025-07-20 21:47:16 +0300

    Тема INNER JOIN

--- training_week_4/SQL/course_sql/base_sql_simple_request.sql
+++ training_week_4/SQL/course_sql/base_sql_simple_request.sql
@@ -248,3 +248,30 @@ FROM customers
 EXCEPT
 SELECT country
 FROM suppliers
+
+-- 31 INNER JOIN - операторов SQL, который объединяет таблицы по общему 
+-- полю, возвращая только те строки, где есть совпадение. INNER JOIN соединяет 
+-- строки из двух таблиц, только если значения в указанных столбцах совпадают. 
+-- Если совпадений нет — такие строки не попадают в результат.
+
+SELECT product_name, suppliers.company_name, units_in_stock
+FROM products
+INNER JOIN suppliers ON products.supplier_id = suppliers.supplier_id
+ORDER BY units_in_stock DESC;
+
+SELECT category_name, SUM(units_in_stock)
+FROM products
+INNER JOIN categories ON products.category_id = categories.category_id
+GROUP BY category_name
+ORDER BY SUM(units_in_stock) DESC
+LIMIT 5;
+
+SELECT category_name, SUM(unit_price * units_in_stock)
+FROM products
+INNER JOIN categories
+ON products.category_id = categories.category_id
+WHERE discontinued != 1
+GROUP BY category_name
+HAVING SUM (unit_price * units_in_stock) > 5000
+ORDER BY SUM(unit_price * units_in_stock) DESC
+
